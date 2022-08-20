c096f3edd6cf2b85132ac0783d50d4202975e7e7
elek8871 <elekmichelle@gmail.com>
2022-08-20 15:11:37 -0600

    adds js outline

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-//console.log ( "test")
+//  needed game buttons:
+const rules = document.getElementById("gameRules")
+const startGame = document.getElementById("gameStart")
+const shuffleTiles = document.getElementById("tileShuffle")
+
+
+// add event listener for tile click
+//  check for match
+// true = either remove tile or leave back side up
+// false = flip tile back over
+// when all tiles are matched display winner msg
+// function to randomize tiles
