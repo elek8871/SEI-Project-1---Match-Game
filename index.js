07d61c1f37b46acb7a980141640c238b2d96915c
elek8871 <elekmichelle@gmail.com>
2022-08-21 14:37:44 -0600

    adds game rules

--- index.js
+++ index.js
@@ -3,22 +3,40 @@ const rules = document.getElementById("gameRules")
 const startGame = document.getElementById("gameStart")
 const shuffleTiles = document.getElementById("tileShuffle")
 
-let tileArray = document.getElementsByClassName("#gameTiles")
-// array of get tiles- not sure how to make the array an empty arrayof tiles
+
+//
+
+let tileArray = document.getElementsByClassName("#tile")
+// array of get tiles- not sure how to make the array an empty array of tiles
 //const tiles = tileArray.length
-//console.log(tiles)
+//console.log(tiles) this gives me a value of 0
 
 // function to randomize tiles
 const randomize = ( )=>{
-    tileArray.sort(() =>Math.random() *0.5)
+    const backImage = getElementsByClassName("#back")
+    backImage.sort(() =>Math.random() *0.5)
     return randomize
-    // console.log(randomize)
+    //console.log(randomize)
 }
-// add event listener for tile click
-const toggleCards = () =>{
-    tileArray.addEventListener("click", )
+const gameTiles = document.getElementsByClassName("#tile")
+
+const flipTiles = () =>{
+    this.classList.toggle ('flip')
+// gameTiles.forEach(tile => tile.addEventListener("click", flipTiles))
+//or
+// gameTiles.forEach(tileArray => tileArray.addEventListener("click", flipTiles))
 }
+
+
 //  check for match
+// if (flipped tiles are ===){
+//     leave backside up
+// } else if !=
+// flipTiles={}
+// if all tiles have matched 
+// display ServiceWorkerContainer
 // true = either remove tile or leave back side up
 // false = flip tile back over
-// when all tiles are matched display winner msg
+
+let displayRules = document.getElementById("gameRules")
+rules.addEventListener("click", displayRules)
