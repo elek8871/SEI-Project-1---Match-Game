641eed23453a023c9e3baef2602a11457cd0d5da
elek8871 <elekmichelle@gmail.com>
2022-08-21 13:34:00 -0600

    adds images and some js functions

--- index.js
+++ index.js
@@ -3,10 +3,22 @@ const rules = document.getElementById("gameRules")
 const startGame = document.getElementById("gameStart")
 const shuffleTiles = document.getElementById("tileShuffle")
 
+let tileArray = document.getElementsByClassName("#gameTiles")
+// array of get tiles- not sure how to make the array an empty arrayof tiles
+//const tiles = tileArray.length
+//console.log(tiles)
 
+// function to randomize tiles
+const randomize = ( )=>{
+    tileArray.sort(() =>Math.random() *0.5)
+    return randomize
+    // console.log(randomize)
+}
 // add event listener for tile click
+const toggleCards = () =>{
+    tileArray.addEventListener("click", )
+}
 //  check for match
 // true = either remove tile or leave back side up
 // false = flip tile back over
 // when all tiles are matched display winner msg
-// function to randomize tiles
