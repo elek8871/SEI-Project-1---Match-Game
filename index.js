bcdde29441c6943ebe99f37c9e3ab9e173cb6bb9
elek8871 <elekmichelle@gmail.com>
2022-08-24 15:38:17 -0600

    adds rules modal

--- index.js
+++ index.js
@@ -6,21 +6,32 @@ document.addEventListener('DOMContentLoaded', () => {
 const startGame = document.getElementById("gameStart")
 const tileShuffle = document.getElementById("shuffle")
 const howToPlay = document.getElementById("gameRules")
-// 
+//modals
+const rulesModal = document.getElementById("rulesModal")
+const rulesSpan = document.getElementsByClassName("close")[0]
+
+// game vars
 const gameTiles = document.querySelectorAll(".tile")
 const gameTilesArray =[...gameTiles]
-const images = document.querySelectorAll(".back")
-const imageArray = [...images]
+const gameScreen = document.getElementById("gameTiles")
 let hasFlippedTile = false
 let firstTile, secondTile
 
-
-// changes text in button to display rules  
-howToPlay.onclick = function rules() {
-   howToPlay.innerText = "Select and two tiles to look for a match. Matching tiles will stay flipped over, otherwise the tiles will flip back. Keep trying until all tiles are matched "
+//rules modal
+howToPlay.onclick = function(){
+   rulesModal.style.display = "block"
 }
+rulesSpan.onclick = function(){
+   rulesModal.style.display = "none"
+}
+window.onclick= function(event){
+   if(event.target === rulesModal){
+   rulesModal.style.display = "none"
+   }
+}
+
 tileShuffle.addEventListener("click",shuffle)
-startGame.addEventListener("click", shuffle)
+
 
 function shuffle(){
    gameTiles.forEach(gameTiles =>{
@@ -74,68 +85,4 @@ function unflipTiles() {
       secondTile.classList.remove("flipTile")
    }, 500)
 }
-
-
-
-
-
-
-
-
-
-
-
-//  function that checks how many cards are clicked then
-// checks for match and flip backover or leave img side up if matched
-
-// function checkMatch (){
-//     const choiceOne = gameTilesArray[0]
-//     const choiceTwo = gameTilesArray[1]
-
-//     if (choiceOne === choiceTwo){
-//         //leave cards overturned
-//     } else {
-//         flip
-//     }
-// }
-// checkMatch()
- 
-
-//if () {
-    //2 tiles are clicked
-
-    // check for match
-
-    // if match -> leave tiles turned
-    
-    // else -> flip tiles
-
-    //check if all tiles are matched
-
- //let randomize = gameTiles
-
-//  function shuffleTiles(array) {
-//     let currentIndex = gameTilesArray.length, temporaryValue, randomIndex;
-//     while (0 !== currentIndex) {
-//       randomIndex = Math.floor(Math.random() * currentIndex);
-//       currentIndex -= 1;
-//       temporaryValue = gameTilesArray[currentIndex];
-//       gameTilesArray[currentIndex] = array[randomIndex];
-//       gameTilesArray[randomIndex] = temporaryValue;
-//     }
-//     return gameTilesArray;
-//   }
-// function shuffleTiles (gameTilesArray) {
-//     gameTilesArray.sort(() => Math.random()- 0.5);
-//  }
-//  shuffleTiles(gameTilesArray)
-//  console.log(shuffleTiles)
- 
-
-
-
-
-
-
 })
-// thanks to https://aadaobi.medium.com/building-a-memory-matching-game-in-javascript-48e792c7b563 for the handy ...gameTiles array method on line 12
